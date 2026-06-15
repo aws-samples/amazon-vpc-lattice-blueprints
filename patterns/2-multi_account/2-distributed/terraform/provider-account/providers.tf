c0a8db4410b5caf353f9960e03dddf644f85fcf9
Pablo Sánchez Carmona <pablosc@amazon.es>
2026-06-15 15:20:59 +0200

    updating multi-account distributed example

--- patterns/2-multi_account/2-distributed/terraform/provider-account/providers.tf
+++ patterns/2-multi_account/2-distributed/terraform/provider-account/providers.tf
@@ -1,7 +1,7 @@
 /* Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
  SPDX-License-Identifier: MIT-0 */
 
-# --- patterns/2-multi_account/2-distributed/terraform/service-account/providers.tf ---
+# --- patterns/2-multi_account/2-distributed/terraform/provider-account/providers.tf ---
 
 terraform {
   required_version = ">= 1.3.0"
@@ -9,6 +9,10 @@ terraform {
     aws = {
       source  = "hashicorp/aws"
       version = ">= 6.27.0"
+    }
+    archive = {
+      source  = "hashicorp/archive"
+      version = ">= 2.7.0"
     }
   }
 }
